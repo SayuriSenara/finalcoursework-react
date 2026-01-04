c9d130cd61fa9d73e20b2395fffadda375e1b9e8
Sayuri Senara <sayuri.20232596@iit.ac.lk>
2026-01-05 00:46:17 +0530

    Add search form structure and load property data

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,14 +1,74 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import propertiesData from "../data/properties.json";
 
 function SearchPage() {
+  const [properties] = useState(propertiesData);
+
+  const [searchCriteria, setSearchCriteria] = useState({
+    type: "Any",
+    minPrice: "",
+    maxPrice: "",
+    minBedrooms: "",
+    maxBedrooms: "",
+    postcode: "",
+  });
+
   return (
     <div>
-      <h2>Search Page</h2>
-      <p>Search form will go here</p>
+      <h2>Search Properties</h2>
 
-      <Link to="/">
-        <button>Back to Home</button>
-      </Link>
+      <form>
+        <label>
+          Property Type:
+          <select>
+            <option value="Any">Any</option>
+            <option value="House">House</option>
+            <option value="Flat">Flat</option>
+          </select>
+        </label>
+
+        <br />
+        <br />
+
+        <label>
+          Min Price:
+          <input type="number" />
+        </label>
+
+        <br />
+        <br />
+
+        <label>
+          Max Price:
+          <input type="number" />
+        </label>
+
+        <br />
+        <br />
+
+        <label>
+          Min Bedrooms:
+          <input type="number" />
+        </label>
+
+        <br />
+        <br />
+
+        <label>
+          Max Bedrooms:
+          <input type="number" />
+        </label>
+
+        <br />
+        <br />
+
+        <label>
+          Postcode Area:
+          <input type="text" placeholder="e.g. NW1" />
+        </label>
+      </form>
+
+      <p>Total properties available: {properties.length}</p>
     </div>
   );
 }
